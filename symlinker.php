f4784576126d40ce855680a63749031f5c9c0bc9
Ercan Ozkaya <ercan@timble.net>
2015-07-10 13:26:38 +0300

    Add custom symlinker rules

---
+++ symlinker.php
@@ -0,0 +1,96 @@
+<?php
+
+use Joomlatools\Console\Command\ExtensionSymlink;
+
+$dependencies = array(
+    'joomlatools-framework' => array('nooku-framework'),
+    'extman'  => array('joomlatools-framework'),
+    'docman'  => array('extman', 'com_files'),
+    'fileman' => array('extman', 'com_files'),
+    'logman'  => array('extman', 'com_activities')
+);
+
+foreach ($dependencies as $project => $deps) {
+    ExtensionSymlink::registerDependencies($project, $deps);
+}
+
+/**
+ * Nooku Framework custom symlinker
+ */
+ExtensionSymlink::registerSymlinker(function($project, $destination, $name, $projects) {
+    // If we are symlinking Koowa, we need to create this structure to allow multiple symlinks in them
+    if (array_intersect(array('nooku-framework', 'joomlatools-framework', 'koowa'), $projects))
+    {
+        $dirs = array($destination.'/libraries/koowa/components', $destination.'/media/koowa');
+        foreach ($dirs as $dir)
+        {
+            if (!is_dir($dir)) {
+                mkdir($dir, 0777, true);
+            }
+        }
+    }
+
+    if (!is_file($project.'/code/koowa.php')) {
+        return false;
+    }
+
+    $vendor_path = $destination.'/vendor';
+
+    if(file_exists($destination.'/composer.json'))
+    {
+        $content  = file_get_contents($destination.'/composer.json');
+        $composer = json_decode($content);
+
+        if(isset($composer->config->{'vendor-dir'})) {
+            $vendor_path = $destination.'/'.$composer->config->{'vendor-dir'};
+        }
+    }
+
+    $code_destination = $vendor_path.'/nooku/nooku-framework';
+
+    if (!is_dir(dirname($code_destination))) {
+        mkdir(dirname($code_destination), 0777, true);
+    }
+
+    if (!file_exists($code_destination)) {
+        `ln -sf $project $code_destination`;
+    }
+
+    $media_source      = $project.'/code/resources/assets';
+    $media_destination = $destination.'/media/koowa/framework';
+
+    if (!file_exists($media_destination)) {
+        `ln -sf $media_source $media_destination`;
+    }
+
+    return true;
+});
+
+
+/**
+ * Nooku components custom symlinker
+ */
+ExtensionSymlink::registerSymlinker(function($project, $destination, $name, $projects) {
+    if (!is_file($project.'/koowa-component.xml')) {
+        return false;
+    }
+
+    $xml       = simplexml_load_file($project.'/koowa-component.xml');
+    $component = 'com_'.$xml->name;
+
+    $code_destination = $destination.'/libraries/koowa/components/'.$component;
+
+    if (!file_exists($code_destination)) {
+        `ln -sf $project $code_destination`;
+    }
+
+    // Special treatment for media files
+    $media = $project.'/resources/assets';
+    $target = $destination.'/media/koowa/'.$component;
+
+    if (is_dir($media) && !file_exists($target)) {
+        `ln -sf $media $target`;
+    }
+
+    return true;
+});
