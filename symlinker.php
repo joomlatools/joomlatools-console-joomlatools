642851a34a5e47a768913ae300c30d0752200e2c
Steven Rombauts <steven@timble.net>
2015-10-06 16:42:19 +0100

    Add verbosity

--- symlinker.php
+++ symlinker.php
@@ -3,6 +3,8 @@
 use Joomlatools\Console\Command\Extension;
 use Joomlatools\Console\Joomla\Util;
 
+use Symfony\Component\Console\Output\OutputInterface;
+
 $dependencies = array(
     'joomlatools-framework' => array('nooku-framework'),
     'extman'  => array('joomlatools-framework'),
@@ -18,14 +20,19 @@ foreach ($dependencies as $project => $deps) {
 /**
  * Nooku Framework custom symlinker
  */
-Extension\Symlink::registerSymlinker(function($project, $destination, $name, $projects) {
+Extension\Symlink::registerSymlinker(function($project, $destination, $name, $projects, OutputInterface $output) {
     // If we are symlinking Koowa, we need to create this structure to allow multiple symlinks in them
     if (array_intersect(array('nooku-framework', 'joomlatools-framework', 'koowa'), $projects))
     {
         $dirs = array(Util::buildTargetPath('/libraries/koowa/components', $destination), Util::buildTargetPath('/media/koowa', $destination));
         foreach ($dirs as $dir)
         {
-            if (!is_dir($dir)) {
+            if (!is_dir($dir))
+            {
+                if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+                    $output->writeln(" * creating empty directory `$dir`");
+                }
+
                 mkdir($dir, 0777, true);
             }
         }
@@ -33,6 +40,10 @@ Extension\Symlink::registerSymlinker(function($project, $destination, $name, $pr
 
     if (!is_file($project.'/code/koowa.php')) {
         return false;
+    }
+
+    if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+        $output->writeln("Symlinking `" . basename($project) . "` into `$destination`");
     }
 
     $vendor_path = $destination.'/vendor';
@@ -53,14 +64,24 @@ Extension\Symlink::registerSymlinker(function($project, $destination, $name, $pr
         mkdir(dirname($code_destination), 0777, true);
     }
 
-    if (!file_exists($code_destination)) {
+    if (!file_exists($code_destination))
+    {
+        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+            $output->writeln(" * creating link `$code_destination` -> `$project`");
+        }
+
         `ln -sf $project $code_destination`;
     }
 
     $media_source      = $project.'/code/resources/assets';
     $media_destination = Util::buildTargetPath('/media/koowa/framework', $destination);
 
-    if (!file_exists($media_destination)) {
+    if (!file_exists($media_destination))
+    {
+        if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+            $output->writeln(" * creating link `$media_destination` -> `$media_source`");
+        }
+
         `ln -sf $media_source $media_destination`;
     }
 
